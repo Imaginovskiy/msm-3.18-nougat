e233c03a0cbefafc3c33b3e4c7a39ad2219e02fa
Steve Kondik <steve@cyngn.com>
2016-07-20 12:48:01 -0700

    qpnp-haptic: Dejank the haptics
     * Add mutexes and call hap_set without the workqueue for direct mode
    
    Change-Id: I98f5b8ddea572def2706ad2cfa18f9dd460757ed

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -329,6 +329,7 @@ struct qpnp_hap {
 	struct qpnp_pwm_info pwm_info;
 	struct mutex lock;
 	struct mutex wf_lock;
+	struct mutex set_lock;
 	struct completion completion;
 	enum qpnp_hap_mode play_mode;
 	enum qpnp_hap_auto_res_mode auto_res_mode;
@@ -1927,6 +1928,8 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 	unsigned long timeout_ns = POLL_TIME_AUTO_RES_ERR_NS;
 	u32 back_emf_delay_us = hap->time_required_to_generate_back_emf_us;
 
+	mutex_lock(&hap->set_lock);
+
 	if (hap->play_mode == QPNP_HAP_PWM) {
 		if (on)
 			rc = pwm_enable(hap->pwm_info.pwm_dev);
@@ -1953,8 +1956,10 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 				qpnp_hap_auto_res_enable(hap, 0);
 
 			rc = qpnp_hap_mod_enable(hap, on);
-			if (rc < 0)
+			if (rc < 0) {
+				mutex_unlock(&hap->set_lock);
 				return rc;
+			}
 
 			rc = qpnp_hap_play(hap, on);
 
@@ -1964,8 +1969,10 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 				usleep_range(back_emf_delay_us,
 							back_emf_delay_us + 1);
 				rc = qpnp_hap_auto_res_enable(hap, 1);
-				if (rc < 0)
+				if (rc < 0) {
+					mutex_unlock(&hap->set_lock);
 					return rc;
+				}
 			}
 			if (hap->act_type == QPNP_HAP_LRA &&
 						hap->correct_lra_drive_freq) {
@@ -1981,8 +1988,10 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 			}
 		} else {
 			rc = qpnp_hap_play(hap, on);
-			if (rc < 0)
+			if (rc < 0) {
+				mutex_unlock(&hap->set_lock);
 				return rc;
+			}
 
 			if (hap->act_type == QPNP_HAP_LRA &&
 						hap->correct_lra_drive_freq) {
@@ -2000,6 +2009,7 @@ static int qpnp_hap_set(struct qpnp_hap *hap, int on)
 		}
 	}
 
+	mutex_unlock(&hap->set_lock);
 	return rc;
 }
 
@@ -2011,6 +2021,7 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 #ifdef CONFIG_LGE_QPNP_HAPTIC_OV_RB
 	dev_dbg(&hap->spmi->dev, "qpnp_hap_td_enable: timeout_ms = %d , voltage = %d\n", value , hap->vmax_mv);
 #endif
+	flush_work(&hap->work);
 
 	mutex_lock(&hap->lock);
 
@@ -2045,7 +2056,10 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
 			      HRTIMER_MODE_REL);
 	}
 	mutex_unlock(&hap->lock);
-	schedule_work(&hap->work);
+	if (hap->play_mode == QPNP_HAP_DIRECT)
+		qpnp_hap_set(hap, hap->state);
+	else
+		schedule_work(&hap->work);
 }
 
 /* play pwm bytes */
@@ -2881,6 +2895,8 @@ static int qpnp_haptic_probe(struct spmi_device *spmi)
 
 	mutex_init(&hap->lock);
 	mutex_init(&hap->wf_lock);
+	mutex_init(&hap->set_lock);
+
 	INIT_WORK(&hap->work, qpnp_hap_worker);
 	INIT_DELAYED_WORK(&hap->sc_work, qpnp_handle_sc_irq);
 	init_completion(&hap->completion);
